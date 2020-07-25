900382deaf3cb016ab114c0f42dbc57a5a086fee
himani <himani.soni@theggis.com>
2020-07-25 17:58:35 +0530

    c-32

--- sketch.js
+++ sketch.js
@@ -7,11 +7,13 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var score = 0;
 
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+   getTime();
+
 }
 
 function setup(){
@@ -45,18 +47,25 @@ function setup(){
 }
 
 function draw(){
+   if(backgroundImg)
     background(backgroundImg);
+
+    textSize(35);
+    text("score: "+ score,width-300,50);
+
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -85,4 +94,20 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getTime(){
+     var response = await fetch ("https://worldtimeapi.org/api/timezone/Asia/Tokyo");
+    var responseJson = await response.json();
+    var day = responseJson.datetime;
+    var hour = day.slice(11,13);
+    console.log(hour);
+
+    if(hour>= 6 && hour < 18){
+        bg = "sprites/bg.png";
+    }
+    else{
+        bg = "sprites/bg2.jpg";
+    }
+    backgroundImg = loadImage(bg);
 }
